7e47562df5d1d46419477ba1a635dc01998a0ce5
josefoygun <josef.oygun@scania.com>
2021-02-28 23:24:46 +0100

    initial

---
+++ worker/index.js
@@ -0,0 +1,38 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+})
+
+const sub = redisClient.duplicate();
+
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2)
+}
+
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)))
+})
+sub.subscribe('insert')
+//
+//
+// 3 - 1 = 2
+// +
+//   3 - 2
+//
+// 2 + 1
+//
+// 2- 1 = 1
+// 1
+//
+// 2 -2 = 0;
+// 1
+//
+//
+// 1 1 2 3 5 8 13
+// 0 1 2 3 4 5 6
+
